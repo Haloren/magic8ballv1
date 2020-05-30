45b901ee2df231587a1e38ed4272a34b07fc08ee
Haloren <mckennakyler@gmail.com>
2020-05-29 21:40:13 -0700

    created views file structure and db tables for users, answer lists, and answers

---
+++ app/controllers/answerlists_controller.rb
@@ -0,0 +1,4 @@
+# class AnswerListsController < ApplicationController
+
+    
+# end
---
+++ db/migrate/20200530042518_create_answerlists.rb
@@ -0,0 +1,8 @@
+class CreateAnswerlists < ActiveRecord::Migration[6.0]
+  def change
+    create_table :answer_lists do |t|
+      t.string :name
+      t.integer :user_id #answer list belongs to a user (user.answer_list will provide lists that belong to a user)
+    end
+  end
+end
