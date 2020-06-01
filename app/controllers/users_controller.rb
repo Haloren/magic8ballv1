ca0684cb8205c44ac3ccd4aafb500488f8ffce76
Haloren <mckennakyler@gmail.com>
2020-06-01 12:12:16 -0700

    created landing routes for users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,24 @@
 class UsersController < ApplicationController
+#get and post for: /login and /create_account
+#get /logout
+    get '/create_account' do
+        "Create Account landing page."
+    end
 
+    post '/create_account' do
+
+    end
+
+    get '/login' do 
+        "Login landing page."
+    end
+
+    post '/login' do
+
+    end
+
+    get '/logout' do
+        "Logout" #redirect to ('/')
+    end
 
 end
