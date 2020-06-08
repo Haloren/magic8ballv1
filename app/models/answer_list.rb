4cb191cda3502183ee6aa80dbb028f1fbad67609
Haloren <mckennakyler@gmail.com>
2020-06-08 12:38:31 -0700

    changed models associations to include answers

--- app/models/answer_list.rb
+++ app/models/answer_list.rb
@@ -1,5 +1,6 @@
 class Answer_List < ActiveRecord::Base
     
     belongs_to :user
+    has_many :answers
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
+    
     has_many :answer_lists
+    
     validates :username, :email, presence: true 
     has_secure_password
+
 end
