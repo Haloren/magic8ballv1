3411365780d6a3649e00911a425722aa06f53e51
Haloren <mckennakyler@gmail.com>
2020-06-09 23:22:15 -0700

    current user is now displayed in layout, moved from user/ask_question

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,12 @@ class UsersController < ApplicationController
     post '/create_account' do
         # binding.pry #check that I have created a user
         # @user = User.create(username: params[:username], email: params[:email], password: params[:password])
-        user = User.create(params) #using all the params for User so don't need to call each individually
+        user = User.create(params) # if using all the User params, you don't need to call each individually
         redirect to "/users/#{user.id}"
         #erb :'users/create_account'  #if fails start again
     end
 
-    get '/users/:id' do
+    get '/users/:id' do #dynamic route with argument of the users_id
         #binding.pry 
         @user = User.find_by(id: params[:id]) #find_by returns "null", find makes an error
         erb :'users/ask_question'
