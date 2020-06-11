9bc7184f777b8deed564dec7cc3c61339274e9c0
Haloren <mckennakyler@gmail.com>
2020-06-11 15:44:15 -0700

    created routes for answer lists and eightball answer

--- app/controllers/answerlists_controller.rb
+++ app/controllers/answerlists_controller.rb
@@ -1,4 +1,16 @@
 class AnswerListsController < ApplicationController
 
+    get '/answer' do
+        erb :'users/eightball_answer'
+    end
     
+    get '/lists' do
+        erb :'answer_lists/create_list'
+        
+    end
+       
+    post '/lists' do
+
+    end
+
 end
