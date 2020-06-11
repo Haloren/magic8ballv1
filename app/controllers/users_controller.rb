3dc677145443dcc61663334a6b967ef9c96acd55
Haloren <mckennakyler@gmail.com>
2020-06-11 10:04:44 -0700

    set up valid? for creating an account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,15 +8,20 @@ class UsersController < ApplicationController
     post '/create_account' do
         # binding.pry #check that I have created a user
         # @user = User.create(username: params[:username], email: params[:email], password: params[:password])
-        user = User.create(params) # if using all the User params, you don't need to call each individually
-        redirect to "/users/#{user.id}"
-        #erb :'users/create_account'  #if fails start again
+        user = User.create(params)
+        if user.valid? 
+            session[:user_id] = user.id # session = user id
+            redirect to "/users/#{user.id}"
+        else 
+            erb :'users/create_account'  # if fails start again
+        end    
+
     end
 
-    get '/users/:id' do #dynamic route with argument of the users_id
-        #binding.pry 
+    get '/users/:id' do # dynamic route with argument of the users_id
+        # binding.pry 
         if User.find_by(id: params[:id])
-            @user = User.find_by(id: params[:id]) #find_by returns "null", find makes an error
+            @user = User.find_by(id: params[:id]) # find_by returns "null", find makes an error
         else
             redirect to '/login'
         end
@@ -24,7 +29,7 @@ class UsersController < ApplicationController
     end
 
     get '/login' do 
-        erb :'users/login' #Login landing page.
+        erb :'users/login' # Login landing page.
         
     end
 
@@ -32,11 +37,11 @@ class UsersController < ApplicationController
         user = User.find_by(username: params[:username])
         user && user.authenticate(params[:password])
         redirect to "/users/#{user.id}"
-        #erb :'users/login'  #if fails start again
+        #erb :'users/login'  # if fails start again
     end
 
     get '/logout' do
-        #destroy/clear session and redirect to ('/')
+        # destroy/clear session and redirect to ('/')
         session.destroy
         redirect to ('/')
     end
