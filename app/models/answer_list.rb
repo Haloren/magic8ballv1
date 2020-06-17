fe0202f3a2e4e27ee246fab80a8af72b8423b5e4
Haloren <mckennakyler@gmail.com>
2020-06-16 18:59:11 -0700

    created seed file for Magic Eight Ball traditional answers.

--- app/models/answer_list.rb
+++ app/models/answer_list.rb
@@ -1,29 +1,6 @@
-class Answer_List < ActiveRecord::Base
+class AnswerList < ActiveRecord::Base
     
     belongs_to :user
     has_many :answers
 
 end
-
-    # list_name = Magic Eight Ball
-    # @answers = ["As I see it, yes.",
-    #     "Ask again later.",
-    #     "Better not tell you now.",
-    #     "Cannot predict now.",
-    #     "Concentrate and ask again.",
-    #     "Don’t count on it.",
-    #     "It is certain.",
-    #     "It is decidedly so.",
-    #     "Most likely.",
-    #     "My reply is no.",
-    #     "My sources say no.",
-    #     "Outlook not so good.",
-    #     "Outlook good.",
-    #     "Reply hazy, try again.",
-    #     "Signs point to yes.",
-    #     "Very doubtful.",
-    #     "Without a doubt.",
-    #     "Yes.",
-    #     "Yes – definitely.",
-    #     "You may rely on it."
-    # ]
